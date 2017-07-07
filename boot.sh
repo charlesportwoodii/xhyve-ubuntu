7123ce9c18019d4f60cc6166543b7fdddf44fbf6
Charles R. Portwood II <charlesportwoodii@erianna.com>
2017-07-07 13:09:22 -0500

    Automatically use n/2 cores for the image

--- boot.sh
+++ boot.sh
@@ -3,7 +3,7 @@
 cd "$(dirname $0)"
 
 MEM="-m 2G"
-SMP="-c 2"
+SMP="-c $(expr $(sysctl -n hw.ncpu) / 2)"
 
 PROGRAM="$(which xhyve)"
 KERNEL="$(ls boot/vmlinuz-* | sort -u | tail -1)"
@@ -12,8 +12,8 @@ CMDLINE="earlyprintk=serial console=ttyS0 acpi=off root=/dev/vda1 ro"
 
 NET="-s 2:0,virtio-net"
 IMG_HDD="-s 4,virtio-blk,./hdd.img"
-PCI_DEV="-s 0:0,hostbridge"
-LPC_DEV=" -s 31,lpc -l com1,stdio"
+PCI_DEV="-s 0:0,hostbridge -s 31,lpc"
+LPC_DEV=" -l com1,stdio"
 
 PROCESS_COUNT=$(ps aux | grep -i xhyve | wc -l)
 if [ $PROCESS_COUNT -gt 1 ]; then
